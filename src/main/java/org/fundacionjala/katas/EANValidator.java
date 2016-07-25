eb025dbf297c833f2677b1a9abcd189d5c29afa9
Mijhail Villarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-07-25 19:03:20 -0400

    updating with quiz EANValidator

---
+++ src/main/java/org/fundacionjala/katas/EANValidator.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.katas;
+
+/**
+ * @author mijhailvillarroel
+ */
+public class EANValidator {
+
+    public static final String REGEX_BLANK = "";
+    public static final int INT_TEN = 10;
+    public static final int INT_ZERO = 0;
+    public static final int ELEMENT_POSITION_12 = 12;
+
+    public static boolean validate(String number) {
+        String[] listElements = number.split(REGEX_BLANK);
+        int result = 0;
+        for (int i = 1; i < listElements.length; i++) {
+            int value = Integer.parseInt(listElements[i - 1]);
+            result += (i % 2 == INT_ZERO && i != INT_ZERO) ? value * 3 : value;
+        }
+        int checksum = result % INT_TEN == INT_ZERO ? INT_ZERO : INT_TEN - (result % INT_TEN);
+        return checksum == Integer.parseInt(listElements[ELEMENT_POSITION_12]);
+    }
+}
